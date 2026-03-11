bf493e3d9e1aaca8f6d276ab6b18f24971d9d5f9
RIIIXCH <sompop@riiixch.com>
2026-03-11 15:51:59 +0700

    feat: Initialize new Vue 3 project with routing, state management, Bootstrap integration, and basic views.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import NavbarMenu from './components/NavbarMenu.vue';
+
+</script>
+
+<template>
+  <NavbarMenu />
+  <router-view />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+#app {
+  font-family: 'Kanit', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: start;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/ContractView.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <div class="container">
+        <div class="alert alert-primary" role="alert">
+            <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ut reiciendis, culpa magnam dolor
+                optio eos laborum odit quo deleniti. Perferendis vitae dolorum blanditiis ipsa ea ducimus, nisi facere
+                amet!
+            </span>
+        </div>
+        <h1>Contract View</h1>
+        <p style="color: blue;">Sompop Iemsombat</p>
+        <span>Welcom to Vue Router</span>
+    </div>
+</template>
+
+<style scoped></style>
